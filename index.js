55c18ca552eee371b7bbde932b800412e4d81d7e
facelessDev <luyosamson@gmail.com>
2024-07-15 16:13:41 +0300

    conclusion of introduction to express

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.listen(
 app.get('/getRequest',(req,res)=>{
     res.send("Hello!this is GET METHOD")
 });
-app.get('/postRequest',(req,res)=>{
+app.post('/postRequest',(req,res)=>{
     res.send("Hello!This is a POST METHOD")
-})
+});
+app.delete('/deleteRequest',(req,res)=>{
+    res.send("Hello!This is s DELETE METHOD")
+});
